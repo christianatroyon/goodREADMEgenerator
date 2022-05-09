7799f730e2cf464c778a3a614c5e1b3d62156f3e
Christiana <christianatroyon@gmail.com>
2022-05-09 17:52:11 -0400

    wrote questions and finished license badge options

---
+++ index.js
@@ -0,0 +1,75 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project."
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "What command should be run to install dependencies?"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?"
+  }];
+
+  function userInput(){
+    return inquirer.prompt(questions)
+  }
+// TODO: Create a function to write README file
+const fileName = "goodREADME.md";
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data, 
+    (err) => err ? console.error(err) : console.log(`Your README has been generated.`))
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    let answers = userInput ();
+    writeToFile((answers.fileName), (generateMarkdown(answers)))
+}
+
+// Function call to initialize app
+init();
+
+
+
+
---
+++ undefined.md
@@ -0,0 +1,3 @@
+# undefined
+## Description
+undefined
